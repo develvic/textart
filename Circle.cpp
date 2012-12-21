958c7c588b942016f69539e9447632c9fe24aed7
olisovyy <olisovyy@lohika.com>
2012-12-21 18:40:30 +0200

    Circle draft added

---
+++ Circle.cpp
@@ -0,0 +1,47 @@
+#include "SampleTextArt.h"
+#include <vector>
+
+class Circle : public TextArtView
+{
+public:
+	Circle()
+		: TextArtView("TextArt-Circle")
+	{
+	}
+
+protected:
+	virtual void getWarpParams(WarpFrameT& warpFrame, SkMatrix& warpMatrix) const
+	{
+		SkPath bSkeleton, tSkeleton;
+
+		SkRect r;
+		r.set(SkIntToScalar(0), SkIntToScalar(0),
+				SkIntToScalar(300), SkIntToScalar(170));
+
+		//bottom
+		{
+			bSkeleton.addOval(r);
+
+			warpFrame.push_back(bSkeleton);
+		}
+
+		//top
+		{
+			r.inset(SkIntToScalar(-120), SkIntToScalar(-40));
+			tSkeleton.addOval(r);
+
+			warpFrame.push_back(tSkeleton);
+		}
+	}
+
+	virtual bool getIsNormalRotated() const 
+	{
+		return true;
+	}
+};
+
+static SkView* MyFactory() {
+    return new Circle;
+}
+
+static SkViewRegister reg(MyFactory);
